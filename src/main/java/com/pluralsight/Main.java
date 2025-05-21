99f0a6be3ba08fea225aab35cca7344274c982a1
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-21 10:45:23 -0700

    Update Main to test app
    -Added Smoothies to the Basket to make sure the code is running smoothly so far

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,6 +2,14 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Basket<Smoothie> smoothieBasket = new Basket<>();
+        smoothieBasket.addItem(new Smoothie("Smoothie Dream",3.50,false,"Strawberry", "Milk", "Sugar"));
+        smoothieBasket.addItem(new Smoothie("Tropical Twist",4.25,true,"Pineapple", "Mango", "Coconut Water"));
+
+        System.out.println("\nSmoothie Basket Receipt:");
+        smoothieBasket.printReceipt();
+        System.out.println("Total Smoothie Basket Pice: $" + String.format("%.2f", smoothieBasket.getTotalPrice()));
+
     }
 }
