488a2ac2ba080bbd487be78ce93a267889e2f8ef
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-21 10:23:19 -0700

    Created Smoothie class
    -Represents a smoothie object; implementing priced item interface
    -Includes details: name, is vegan, ingredients, etc

---
+++ src/main/java/com/pluralsight/Smoothie.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Smoothie implements PricedItem{
+    private String name;
+    private double price;
+    private boolean isVegan;
+    private List<String> ingredients;
+
+    public Smoothie(String name, double price, boolean isVegan, String... ingredients) {
+        this.name = name;
+        this.price = price;
+        this.isVegan = isVegan;
+        this.ingredients = Arrays.asList(ingredients);
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    public boolean isVegan() {
+        return isVegan;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return name + " ($" + String.format("%.2f" , price) + (isVegan ? "Vegan" : "") + ", Ingredients: " + ingredients + ")";
+    }
+}
