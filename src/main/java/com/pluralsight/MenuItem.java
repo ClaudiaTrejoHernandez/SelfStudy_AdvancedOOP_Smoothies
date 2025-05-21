5486ad0087ab323ab31f9ad95ea2bb4ebd9fea82
ClaudiaTrejoHernandez <claudia.s.trejo@gmail.com>
2025-05-21 10:21:19 -0700

    Updated Menu Item class
    -Generic class
    -Will work with any object type
    -Reusable

--- src/main/java/com/pluralsight/MenuItem.java
+++ src/main/java/com/pluralsight/MenuItem.java
@@ -1,4 +1,19 @@
 package com.pluralsight;
 
-public class MenuItem {
+public class MenuItem<T> {
+    private String label;
+    private T product;
+
+    public MenuItem(String label, T product) {
+        this.label = label;
+        this.product = product;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public T getProduct() {
+        return product;
+    }
 }
